496de9e2dc6d8432cec22f18216394ff8bc80867
Igor Kodzhebash <igorkodzebash@gmail.com>
2022-03-31 21:34:40 +0300

    fix all problems

--- script.js
+++ script.js
@@ -1,37 +1,17 @@
+'use strict'
 let userAge = +prompt("Сколько тебе лет");
 // let userAge = '15';
 
 
-if (typeof userAge === 'number') {
-    if (userAge % 100 <= 20 ) {
-        if (userAge === 0 || userAge >= 5 && userAge < 21) {
-            alert(`тебе ${userAge} лет`);
-        } else if (userAge >= 2 && userAge <= 4) {
-            alert(`тебе ${userAge} года`);
-        } else if (userAge == 1) {
-            alert(`тебе ${userAge} год`);
-        } else {
-            alert(`тебе ${userAge} лет`);
-        }
-    } else if (userAge % 100 > 20) {
-        if (userAge === 0 || userAge % 10 >= 5 && userAge % 10 < 21) {
-            alert(`тебе ${userAge} лет`);
-        } else if (userAge % 10 >= 2 && userAge % 10 <= 4) {
-            alert(`тебе ${userAge} года`);
-        } else if (userAge % 10 == 1) {
-            alert(`тебе ${userAge} год`);
-        } else {
-            alert(`тебе ${userAge} лет`);
-        }
-
-
-    }
+if (!isFinite(userAge) || userAge == '' || userAge == null) {
+    alert(`Введите номер`);
+} else if (userAge == 0 || userAge % 100 >=5 && userAge % 100 <= 20 ) {
+        alert(`Вам ${userAge} лет`); 
+    } else if (userAge % 10 >= 2 && userAge % 10 <= 4 ) {
+        alert(`Вам ${userAge} года`);
+    } else if (userAge % 10 === 1) {
+        alert(`Вам ${userAge} год`);
+    } else {
+        alert(`Вам ${userAge} лет`);
     
-} else {
-    console.log("Введите цифру");
-}
-
-
-// console.log(2 % 100);
-
-
+};
