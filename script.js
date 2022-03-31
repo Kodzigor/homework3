e769bdecf88dfcf16dec54aeee88e6ac24fc8c37
Igor Kodzhebash <igorkodzebash@gmail.com>
2022-03-31 18:36:09 +0300

    final solution

--- script.js
+++ script.js
@@ -3,21 +3,28 @@ let userAge = +prompt("Сколько тебе лет");
 
 
 if (typeof userAge === 'number') {
-    if (userAge === 0 || userAge > 4 && userAge <= 20 ) {
-        console.log(`Тебе дружок ${userAge} лет`); 
-    } else if (userAge > 20) {
-        if((userAge % 10) === 0 || (userAge % 10) > 4 && (userAge % 10) <=10 ) {
-            console.log(`Тебе дружок ${userAge} лет`);
-        } else if ((userAge % 10) === 1) {
-            console.log(`Тебе дружок ${userAge} год`);
-        } else if ((userAge % 10) >= 2 && (userAge % 10) <= 4) {
+    if (userAge < 100) {
+        if (userAge === 0 || userAge > 4 && userAge <= 20 ) {
+            console.log(`Тебе дружок ${userAge} лет`); 
+        } else if (userAge % 10 == 1) {
+            console.log(`Тебе дружок ${userAge} год`);   
+        } else if (userAge % 10 >= 2 && userAge % 10 <= 4) {
             console.log(`Тебе дружок ${userAge} года`);
+        } else {
+            console.log(`Тебе дружок ${userAge} лет`);
         }
-    } else if (userAge >= 2 && userAge <= 4) {
-        console.log(`Тебе дружок ${userAge} года`);
-    } else {
-        console.log(`Тебе дружок ${userAge} год`);
+    } else if (userAge > 100) {
+        if (userAge % 100 == 1) {
+            console.log(`Тебе дружок ${userAge} год`);   
+        } else if (userAge % 100 >= 2 && userAge % 100 <= 4) {
+            console.log(`Тебе дружок ${userAge} года`);
+        } else if (userAge % 100 >=5 || userAge % 100 <=20) {
+            console.log(`Тебе дружок ${userAge} лет`);
+        }
     }
+    
 } else {
-    console.log(`Введите число`);
-} 
+    console.log("Введите цифру");
+}
+
+
